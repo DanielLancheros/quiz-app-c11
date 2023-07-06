f9a4706685fb0b3983c0f6384c541a002b551586
Daniel Lancheros <daniellancheros@Daniels-MacBook-Air.local>
2023-07-06 12:25:59 -0400

    added footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
 import './styles/App.css';
 
 function App() {
   return (
-  <>
-  <h1>TEST</h1>
-  </>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element= { <Home />}/>
+    </Routes>
+  </BrowserRouter>
+  
   );
 }
 
